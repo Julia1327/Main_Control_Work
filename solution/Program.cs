a2d3b3ce6f71257774efc4962a85386b606c7243
«Julia» <juliya454@yandex.ru>
2024-02-06 15:26:53 +0300

    Загрузили решение задачи

--- solution/Program.cs
+++ solution/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+string[] strings = {"Hello", "2", "world", ":-)"};
+string[] strings_1 = new string[strings.Length];
+int outputIndex = 0;
+
+for (int i = 0; i < strings.Length; i++)
+{
+    if (strings[i].Length <= 3)
+    {
+        strings_1[outputIndex++] = strings[i];
+    }
+}
+
+for (int i = 0; i < outputIndex; i++)
+{
+    Console.WriteLine(strings_1[i]);
+}
